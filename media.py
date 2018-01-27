70529a87ecdb4a3ba9c5f670973dc0dc3ae50174
zziro <zziro.123@gmail.com>
2018-01-27 18:29:55 -0500

    Documentation

--- media.py
+++ media.py
@@ -2,17 +2,32 @@ import webbrowser
 
 
 class Movie():
-    def __init__(self, movie_title, director, movie_storyline, poster_image,
+    """Class describing attributes an behavior of a Movie.
+
+    Args:
+        title           (str): Movie's title.
+        director        (str): Movie's director.
+        movie_storyline (str): Movie's storyline.
+        poster_image    (str): Movie's film poster.
+        trailer_youtube (str): Movie's trailer on youtube.
+
+    Atributes:
+        title           (str): Movie's title.
+        director        (str): Movie's director.
+        movie_storyline (str): Movie's storyline.
+        poster_image    (str): Movie's film poster.
+        trailer_youtube (str): Movie's trailer on youtube.
+
+    Methods:
+        show_trailer (self): Open default web browser 
+    """
+    def __init__(self, title, director, movie_storyline, poster_image,
                  trailer_youtube):
-
-        # Instance variable to store all propiertes about the movie
-
-        self.title = movie_title
+        self.title = title
         self.director = director
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    # Instance method to open the trailer on youtube
-    def show_trailer(self):
+    
+    def show_trailer(self):        
         webbrowser.open(self.trailer_youtube_url)
